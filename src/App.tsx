1018fc161576ddf4cff8dfb274416cfab4b7bfac
Joel Loh Quan Yu <xolotlwastaken@gmail.com>
2026-09-08 21:35:19 +0800

    refactor: remove hyphen from Steven Paul Jobs marquee

--- src/App.tsx
+++ src/App.tsx
@@ -57,19 +57,19 @@ export default function App() {
           {/* First Half */}
           <div className="flex shrink-0 items-center">
             <span className="inline-block pr-8 sm:pr-14">
-              Steven Paul &mdash; Jobs
+              Steven Paul Jobs
             </span>
             <span className="inline-block pr-8 sm:pr-14">
-              Steven Paul &mdash; Jobs
+              Steven Paul Jobs
             </span>
           </div>
           {/* Second Half (Identical for seamless infinite continuous loop) */}
           <div className="flex shrink-0 items-center">
             <span className="inline-block pr-8 sm:pr-14">
-              Steven Paul &mdash; Jobs
+              Steven Paul Jobs
             </span>
             <span className="inline-block pr-8 sm:pr-14">
-              Steven Paul &mdash; Jobs
+              Steven Paul Jobs
             </span>
           </div>
         </div>
